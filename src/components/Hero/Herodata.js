0099e937caeb6b7fbc444fbfc8c59b7215a9fa73
iamawez <awezmanyar1672@gmail.com>
2024-11-02 22:01:00 +0530

    website update

--- src/components/Hero/Herodata.js
+++ src/components/Hero/Herodata.js
@@ -9,7 +9,7 @@ export const heroData = {
       " Yoga is a way of life, rather than a chore. Counteract the stresses of modern life by becoming more mindful and compassionate.",
     title1: "Yoga to",
     title2: "Release Stress",
-    btn1: "Book Now",
-    btn2: "Get Started",
+    btn1: "Book Now ",
+    btn2: "Get Started ",
     image: HeroImage,
   };
